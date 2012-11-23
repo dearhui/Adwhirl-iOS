1f156df5e94373e47b66071ddb583d713ea89ce4
Ming Hui Ho <dearhui@gmail.com>
2012-11-23 14:16:17 +0800

    修正 Vpon adapter 以支援新的 vpon sdk

--- AdNetwork/MyAdapters/AdWhirlAdapterVpon.m
+++ AdNetwork/MyAdapters/AdWhirlAdapterVpon.m
@@ -25,9 +25,8 @@
 }
 
 #pragma mark - 
-- (void)getAd {    
-    [VponAdOn initializationLatitude:0 longtitude:0 platform:TW];
-    
+- (void)getAd {
+    [VponAdOn initializationPlatform:TW];
     [[VponAdOn sharedInstance] setIsVponLogo:YES];
     
     UIViewController *display = [[VponAdOn sharedInstance] adwhirlRequestDelegate:self licenseKey:[self publisherId] size:ADON_SIZE_320x48];
@@ -61,19 +60,33 @@
     }
 #endif
 }
+
+// 回傳Vpon廣告抓取成功
+- (void)onClickAd:(UIViewController *)bannerView withValid:(BOOL)isValid withLicenseKey:(NSString *)adLicenseKey
+{
+    if (isValid == YES)
+    {
+        NSLog(@"Vpon廣告有效點擊:%@:%@",bannerView ,adLicenseKey);
+    } else {
+        NSLog(@"Vpon廣告無效點擊 也許已經點擊過了:%@:%@",bannerView ,adLicenseKey);
+    }
+}
+
 - (void)onRecevieAd:(UIViewController *)bannerView withLicenseKey:(NSString *)licenseKey {
-#ifdef DEBUG
-    NSLog(@"Vpon廣告抓取成功:%@:%@",bannerView ,licenseKey);
-#endif
-    [adWhirlView adapter:self didReceiveAdView:bannerView.view];
+    #ifdef DEBUG
+        NSLog(@"Vpon廣告抓取成功:%@:%@",bannerView ,licenseKey);
+    #endif
     
+    [adWhirlView adapter:self didReceiveAdView:bannerView.view];
 }
 - (void)onFailedToRecevieAd:(UIViewController *)bannerView withLicenseKey:(NSString *)licenseKey {
-#ifdef DEBUG
-    NSLog(@"Vpon廣告抓取失敗:%@:%@",bannerView ,licenseKey);
-#endif
+    #ifdef DEBUG
+        NSLog(@"Vpon廣告抓取失敗:%@:%@",bannerView ,licenseKey);
+    #endif
+    
     [adWhirlView adapter:self didFailAd:nil];
 }
+
 
 #pragma mark - pubId
 
